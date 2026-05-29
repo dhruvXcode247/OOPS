8882627c0c42725f3ca49a785bd7933ef3a85f08
Dhruv Sharma <d.sharma200611@gmail.com>
2026-05-30 01:19:07 +0530

    Added one file

---
+++ hierarchialinheritance.cpp
@@ -0,0 +1,68 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Human {
+
+    protected:
+    string name;
+    int age;
+
+    public:
+
+    Human () {
+
+    };
+
+    Human(string name, int age) {
+        this->name=name;
+        this->age=age;
+    }
+
+    void display() {
+        cout << name << " " << age <<" \n";
+    }
+
+    void work() {
+        cout << "I am working \n";
+    }
+};
+
+class Student:public Human {
+    int roll_number,fees;
+
+    public:
+    Student(string name, int age, int roll_number, int fees): Human(name,age) {
+        this->roll_number=roll_number;
+        this->fees=fees;
+    }
+
+    void display() {
+        cout << name << " " << age << roll_number << " " << fees << endl;
+    }
+};
+
+class Teacher: public Human {
+
+    int salary;
+
+    public:
+    Teacher(int salary, string name, int age) {
+        this->name=name;
+        this->salary=salary;
+        this->age=age;
+    }
+
+    void display() {
+        cout << name << " " << age << " " << " " << fees << endl; 
+    }
+};
+
+int main() {
+
+    Student A1("Rohit",12,10,99);
+    A1.display();
+    Teacher A2(99,"Mohit",23);
+    A2.display();
+
+    return 0;
+}
